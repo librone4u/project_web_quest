99104ef6e94827e40bcdc551e5d0d48653af8572
”Volodymyr” <snehvova@gmail.com>
2024-06-27 15:18:55 +0200

    add question and conclusion classes

--- src/main/java/com.project_web_quest/model/Conclusion.java
+++ src/main/java/com.project_web_quest/model/Conclusion.java
@@ -1,4 +1,17 @@
 package com.project_web_quest.model;
 
-public class Conclusion {
+import lombok.Data;
+
+import java.net.URI;
+
+@Data
+public class Conclusion implements GameElement{
+    private String name;
+    private String finalText;
+    private URI backGround;
+    public Conclusion(String name,String finalText, URI backGround) {
+        this.name = name;
+        this.finalText = finalText;
+        this.backGround = backGround;
+    }
 }
--- src/main/java/com.project_web_quest/model/Question.java
+++ src/main/java/com.project_web_quest/model/Question.java
@@ -1,4 +1,14 @@
 package com.project_web_quest.model;
 
-public class Question {
+import lombok.Data;
+
+import java.util.Map;
+@Data
+public class Question implements GameElement{
+    private String question;
+    private Map<String, String> answers;
+    public Question(String question, Map<String, String> answers) {
+        this.question = question;
+        this.answers = answers;
+    }
 }
